63ee7b55b53030fd6a141b88c774e2b96e2c2ce1
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-10-21 00:39:07 -0300

    feat: add json de livros + recuperação na controller

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,7 +1,9 @@
+import bdFake from "../livros.json" assert { type: "json" };
+
 class LivroController {
   static async listarLivros(req, res) {
     try {
-      res.send("olá mundão Plana");
+      res.status(200).json(bdFake);
     } catch (error) {
       res.status(500).send(error);
     }
